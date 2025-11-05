e17fe46080665e1a9a2ce0f68efc65dee8d4342e
Edgar Ramazyan <18edox@gmail.com>
2025-11-05 22:50:31 +0400

    Data types in terraform

---
+++ 4.Read Generate Modify Configurations/15. Data types/data_types.tf
@@ -0,0 +1,14 @@
+variable "username" {
+    type = number
+  
+}
+
+provider "aws" {
+    region = "us-east-1"
+    shared_credentials_files = ["../../aws.key"]
+    profile = "customprofile"
+}
+
+resource "aws_iam_user" "lb" {
+  name = var.username
+}
